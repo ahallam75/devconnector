0b194ec0e919f7ec8a16d9bd8bab73d934980e2a
ahallam <alexhallam75@gmail.com>
2019-09-05 10:17:34 -0400

    Working on environment variables for keys

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const config = require("config");
+const keys = require("../../config/keys");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
