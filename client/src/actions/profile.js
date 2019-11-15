98dc9188698beb6d89799fbb4a346072a7935acd
ahallam <alexhallam75@gmail.com>
2019-11-15 13:01:22 -0500

    Starting on dashboard

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
 
-// Get current users profile
+// Get current user profile
 export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get("/api/profile/me");
