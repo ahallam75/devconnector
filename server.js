499ebf49010a3f73a776ca5058ad49e78df43c27
ahallam <alexhallam75@gmail.com>
2019-11-19 12:55:59 -0500

    Fixed server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "keys_prod") {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
